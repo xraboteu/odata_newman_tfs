531deae03012acdd409b5e46cb9080da868e2062
Xavier <xavier841@hotmail.com>
2017-12-03 17:45:35 +0100

    first commit

---
+++ AdventureWorks/AdventureWorks/App_Start/WebApiConfig.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.OData.Builder;
+using System.Web.Http.OData.Extensions;
+using AdventureWorks.Models;
+
+namespace AdventureWorks
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Configuration et services API Web
+            ODataConventionModelBuilder builder = new ODataConventionModelBuilder();
+            builder.EntitySet<Employee>("Employees");
+            config.Routes.MapODataServiceRoute("odata", "odata", builder.GetEdmModel());
+            // Itinéraires de l'API Web
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ AdventureWorks/AdventureWorks/Controllers/EmployeesController.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.ModelBinding;
+using System.Web.Http.OData;
+using System.Web.Http.OData.Routing;
+using AdventureWorks.Models;
+
+namespace AdventureWorks.Controllers
+{
+    /*
+    La classe WebApiConfig peut exiger d'autres modifications pour ajouter un itinéraire à ce contrôleur. Fusionnez ces instructions dans la méthode Register de la classe WebApiConfig, le cas échéant. Les URL OData sont sensibles à la casse.
+
+    using System.Web.Http.OData.Builder;
+    using System.Web.Http.OData.Extensions;
+    using AdventureWorks.Models;
+    ODataConventionModelBuilder builder = new ODataConventionModelBuilder();
+    builder.EntitySet<Employee>("Employees");
+    config.Routes.MapODataServiceRoute("odata", "odata", builder.GetEdmModel());
+    */
+    public class EmployeesController : ODataController
+    {
+        private AdventureWorks2012Entities db = new AdventureWorks2012Entities();
+
+        // GET: odata/Employees
+        [EnableQuery]
+        public IQueryable<Employee> GetEmployees()
+        {
+            return db.Employee;
+        }
+
+        // GET: odata/Employees(5)
+        [EnableQuery]
+        public SingleResult<Employee> GetEmployee([FromODataUri] int key)
+        {
+            return SingleResult.Create(db.Employee.Where(employee => employee.BusinessEntityID == key));
+        }
+
+        // PUT: odata/Employees(5)
+        public IHttpActionResult Put([FromODataUri] int key, Delta<Employee> patch)
+        {
+            Validate(patch.GetEntity());
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            Employee employee = db.Employee.Find(key);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            patch.Put(employee);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EmployeeExists(key))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return Updated(employee);
+        }
+
+        // POST: odata/Employees
+        public IHttpActionResult Post(Employee employee)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Employee.Add(employee);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (EmployeeExists(employee.BusinessEntityID))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return Created(employee);
+        }
+
+        // PATCH: odata/Employees(5)
+        [AcceptVerbs("PATCH", "MERGE")]
+        public IHttpActionResult Patch([FromODataUri] int key, Delta<Employee> patch)
+        {
+            Validate(patch.GetEntity());
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            Employee employee = db.Employee.Find(key);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            patch.Patch(employee);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EmployeeExists(key))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return Updated(employee);
+        }
+
+        // DELETE: odata/Employees(5)
+        public IHttpActionResult Delete([FromODataUri] int key)
+        {
+            Employee employee = db.Employee.Find(key);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            db.Employee.Remove(employee);
+            db.SaveChanges();
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool EmployeeExists(int key)
+        {
+            return db.Employee.Count(e => e.BusinessEntityID == key) > 0;
+        }
+    }
+}
